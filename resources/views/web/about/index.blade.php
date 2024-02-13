c415fac2b2cd72a2a54e96b0c8308ae268e0354a
Mohmadkahal22 <mohmadalkahal@gmail.com>
2024-02-13 15:09:42 -0500

    Front
    Update contact and about

--- resources/views/web/about/index.blade.php
+++ resources/views/web/about/index.blade.php
@@ -1,85 +1,84 @@
 @extends('web.weblayout')
 @section('content')
-    <div class="contact">
-        <div class="contactus">
-            <h1>About EZM</h1>
-        </div>
-        <div class="contactus">
-            <p>
-                Welcome to our platform, where we are passionate about empowering
-                individuals to master the world of design and development. We offer a
-                wide range of online courses designed to equip learners with the
-                skills and knowledge needed to succeed in the ever-evolving digital
-                landscape.
-            </p>
-        </div>
+<div class="contact">
+    <div class="contactus">
+        <h1>About EZM</h1>
     </div>
-    <div class="Achievements">
-        <div class="contactus">
-            <h1>Achievements</h1>
-            <p>
-                Our commitment to excellence has led us to achieve significant
-                milestones along our journey. Here are some of our notable
-                achievements
-            </p>
-        </div>
+    <div class="contactus">
+        <p>
+            Welcome to our platform, where we are passionate about empowering
+            individuals to master the world of design and development. We offer a
+            wide range of online courses designed to equip learners with the
+            skills and knowledge needed to succeed in the ever-evolving digital
+            landscape.
+        </p>
     </div>
-    <div class="contact">
-        @foreach ($achievement as $achievements)
-            <div  class="contactdescription">
-                <logo>LOGO</logo>
-                <h3>{{ $achievements->title }}</h3>
-                <p>
-                    {{ $achievements->achievement }}
-                </p>
-            </div>
-        @endforeach
+</div>
+<div class="Achievements">
+    <div class="contactus">
+        <h1>Achievements</h1>
+        <p>
+            Our commitment to excellence has led us to achieve significant
+            milestones along our journey. Here are some of our notable
+            achievements
+        </p>
+    </div>
+</div>
+<div class="contact">
+    @foreach ($achievement as $achievements)
+    <div class="contactdescription">
+        <logo>LOGO</logo>
+        <h3>{{ $achievements->title }}</h3>
+        <p>
+            {{ $achievements->achievement }}
+        </p>
+    </div>
+    @endforeach
 
+</div>
+<div class="d-flex justify-content-center">
+</div>
+<div class="Achievements">
+    <div class="contactus">
+        <h1>Our Goals</h1>
+        <p>
+            At SkillBridge, our goal is to empower individuals from all
+            backgrounds to thrive in the world of design and development. We
+            believe that education should be accessible and transformative,
+            enabling learners to pursue their passions and make a meaningful
+            impact. Through our carefully crafted courses, we aim to
+        </p>
     </div>
-    <div class="d-flex justify-content-center">
-        <div class="container mt-4">{{ $achievement->onEachSide(2)->links() }}</div>
+</div>
+<div class="contact">
+    @foreach ($goal as $goals)
+    <div class="contactdescription">
+        <logo>LOGO</logo>
+        <h3>{{ $goals->title }}</h3>
+        <p>
+            {{ $goals->golas }}
+        </p>
     </div>
-    <div class="Achievements">
-            <div class="contactus">
-                <h1>Our Goals</h1>
-                <p>
-                    At SkillBridge, our goal is to empower individuals from all
-                    backgrounds to thrive in the world of design and development. We
-                    believe that education should be accessible and transformative,
-                    enabling learners to pursue their passions and make a meaningful
-                    impact. Through our carefully crafted courses, we aim to
-                </p>
-            </div>
-        </div>
-        <div class="contact">
-            @foreach ($goal as $goals)
-                <div class="contactdescription">
-                    <logo>LOGO</logo>
-                    <h3>{{ $goals->title }}</h3>
-                    <p>
-                        {{ $goals->golas }}
-                    </p>
-                </div>
-            @endforeach
-        </div>
-        <div class="Together">
-            <div class="contactus">
-              <h1>
-                <span>Together</span>, let s shape the future of digital innovation
-              </h1>
-              <p>
-                Join us on this exciting learning journey and unlock your potential in
-                design and development.
-              </p>
-            </div>
-            <div class="contactus">
-              <a href="{{ route('register') }}">Join Now</a>
-            </div>
-            <br>
-            <br>
+    @endforeach
+</div>
+<div class="Together">
+    <div class="contactus">
+        <h1>
+            <span>Together</span>, let s shape the future of digital innovation
+        </h1>
+        <p>
+            Join us on this exciting learning journey and unlock your potential in
+            design and development.
+        </p>
+    </div>
+    <div class="contactus">
+        <a href="{{ route('register') }}">Join Now</a>
+    </div>
+    <br>
+    <br>
 
-        </div>
-    @endsection
+</div>
+@endsection
 <style>
     .contact {
         background-color: #eeeeee;
@@ -90,29 +89,41 @@
         grid-template-columns: 1fr 1fr;
         padding-top: 40px;
         gap: 35px;
-      }
-      .contactus {
+
+    }
+
+    .contactus {
         padding-left: 40px;
         padding-top: 40px;
-      }
-      .contactdescription {
+
+    }
+
+    .contactdescription p {
+        font-size: 12px;
+    }
+
+    .contactdescription {
         background-color: white;
         padding-left: 40px;
         gap: 20px;
         font-size: 16px;
         padding-top: 40px;
-      }
-      .Achievements {
+        width: 37vw;
+        height: 17vw;
+    }
+
+    .Achievements {
         background-color: #eeeeee;
         display: flex;
         justify-content: space-between;
         align-items: center;
         padding: 0 130px;
-        display: grid;
         padding-top: 40px;
         padding-bottom: 40px;
-      }
-      .Together {
+
+    }
+
+    .Together {
         background-color: #eeeeee;
         justify-content: space-between;
         align-items: center;
@@ -120,11 +131,13 @@
         display: grid;
         grid-template-columns: 1.9fr 1fr;
         padding-top: 40px;
-      }
-      h1 span {
+    }
+
+    h1 span {
         color: #00aeef;
-      }
-      .contactus a {
+    }
+
+    .contactus a {
         text-decoration: none;
         background-color: #00aeef;
         color: white;
@@ -132,6 +145,5 @@
         font-size: 15px;
         padding: 10px;
         margin-left: 100px;
-      }
-
+    }
 </style>
--- resources/views/web/contact/index.blade.php
+++ resources/views/web/contact/index.blade.php
@@ -1,86 +1,77 @@
 @extends('web.weblayout')
 @section('content')
 <div class="contact">
-    <div class="contactus"><h1>Contact Us</h1></div>
-    <div class="contactdescription">
-      <p>
-        Welcome to SkillBridge's Pricing Plan page, where we offer two
-        comprehensive options to cater to your needs: Free and Pro. We believe
-        in providing flexible and affordable pricing options for our services.
-        Whether you're an individual looking to enhance your skills or a
-        business seeking professional development solutions, we have a plan
-        that suits you. Explore our pricing options below and choose the one
-        that best fits your requirements.
-      </p>
+    <div class="contactus">
+        <h1>Contact Us</h1>
     </div>
-  </div>
-  <form action={{  route('store.contact')}} method="post">
+    <div class="contactdescription">
+        <p>
+            Welcome to SkillBridge's Pricing Plan page, where we offer two
+            comprehensive options to cater to your needs: Free and Pro. We believe
+            in providing flexible and affordable pricing options for our services.
+            Whether you're an individual looking to enhance your skills or a
+            business seeking professional development solutions, we have a plan
+            that suits you. Explore our pricing options below and choose the one
+            that best fits your requirements.
+        </p>
+    </div>
+</div>
+<form action={{ route('store.contact') }} method="post">
     @csrf
     @method('POST')
     <div class="contactussubmet">
-      <div class="communication">
-        <div class="table">
-          <div class="cell">
-            <div class="row"><p>First Name</p></div>
-            <div class="row">
-              <input type="text"   name="name" placeholder="Enter First Name"  />
+        <div class="communication">
+            <div class="table">
+                <div class="cell">
+                    <div class="row">
+                        <p>First Name</p>
+                    </div>
+                    <div class="row">
+                        <input type="text" name="name" placeholder="Enter First Name" />
+                    </div>
+                    <br>
+                    <div class="row">
+                        <p>Email</p>
+                    </div>
+                    <div class="row">
+                        <input type="email" name="email" id="" placeholder="Enter Your Email" />
+                    </div>
+                    <br>
+                </div>
             </div>
-            <br>
-            <div class="row"><p>Email</p></div>
-            <div class="row">
-              <input
-                type="email"
-                name="email"
-                id=""
-                placeholder="Enter Your Email"
-              />
+            <div>
+                <p>Subject</p>
+                <input type="text" name="subject" placeholder="Enter Your Subject" id="inputsubject" />
+                <p>Message</p>
+                <textarea name="discription" placeholder="Enter Your Message Here ..." id="inputmessage" cols="30"
+                    rows="10"></textarea>
             </div>
-            <br>
-          </div>
-        </div>
+            <div class="submet">
+                <button class="submit" type="submit">submit</button>
+            </div>
+</form>
+</div>
+<div class="add">
+    <div class="email">
+        <logo><img src="{{ asset('logo.jpg') }}" alt="" width="40px" /></logo>
+        <p>{{ DB::table('webs')->where('id', 1)->value('gmail') }}</p>
+    </div>
+    <div class="phone">
+        <logo><img src="{{ asset('logo.jpg') }}" alt="" width="40px" /></logo>
+        <p>{{ DB::table('webs')->where('id', 1)->value('phone') }}</p>
+    </div>
+    <div class="socialmedia">
+        <logo><img src="{{ asset('logo.jpg') }}" alt="" width="40px" /></logo>
         <div>
-          <p>Subject</p>
-          <input
-            type="text"
-            name="subject"
-            placeholder="Enter Your Subject"
-            id="inputsubject"
-          />
-          <p>Message</p>
-          <textarea
-            name="discription"
-            placeholder="Enter Your Message Here ..."
-            id="inputmessage"
-            cols="30"
-            rows="10"
-          ></textarea>
-        </div>
-        <div class="submet">
-            <button type="submit">submit</button>
-        </div>
-    </form>
-      </div>
-      <div class="add">
-        <div class="email">
-          <logo><img src="{{ asset('logo.jpg') }}" alt="" width="40px" /></logo>
-          <p>{{ DB::table('webs')->where('id', 1)->value('gmail') }}</p>
-        </div>
-        <div class="phone">
-          <logo><img src="{{ asset('logo.jpg') }}" alt="" width="40px" /></logo>
-          <p>{{ DB::table('webs')->where('id', 1)->value('phone') }}</p>
-        </div>
-        <div class="socialmedia">
-          <logo><img src="{{ asset('logo.jpg') }}" alt="" width="40px" /></logo>
-          <div>
             <a href="{{ DB::table('webs')->where('id', 1)->value('facebook') }}"> facebook</a>
             <br>
             <br>
             <a href="{{ DB::table('webs')->where('id', 1)->value('linkedin') }}"> linkedin</a>
         </div>
-        </div>
-      </div>
     </div>
-  </form>
+</div>
+</div>
+</form>
 @endsection
 <style>
     .contact {
@@ -88,97 +79,128 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 0 160px;
+        padding: 0 100px;
         display: grid;
         grid-template-columns: 1fr 1fr;
         padding-top: 40px;
-      }
-      .contactus {
+    }
+
+    .contactus {
         padding-left: 40px;
         padding-top: 40px;
-      }
-      .contactdescription p {
-        font-size: 16px;
+    }
+
+    .contactdescription p {
+        font-size: 10px;
         padding-top: 40px;
-      }
-      .contactussubmet {
-        background-color: #eeeeee;
-        display: flex;
+    }
+
+    .contactussubmet {
         justify-content: space-between;
         align-items: center;
         padding: 0 160px;
         display: grid;
         grid-template-columns: 2fr 1fr;
         padding-top: 80px;
-      }
-      .communication {
-        margin-bottom: 150px;
-      }
-      .table {
+        background-color: #eeeeee;
+    }
+
+    .communication {
+        background-color: white;
+        padding: 40px;
+        padding-top: 100px;
+        padding-bottom: 100px;
+    }
+
+    .table {
         display: table;
-      }
-      .row {
+    }
+
+    .row {
         display: table-row;
-      }
-      .cell {
+    }
+
+    .cell {
         display: table-cell;
-      }
-      input[type="text"],
-      input[type="email"] {
+    }
+
+    input[type="text"],
+    input[type="email"] {
         padding: 7px;
         font-size: 16px;
         border: none;
-        width: 360px;
+        width: 260px;
         border-radius: 5px;
         text-align: left;
-      }
-      #inputsubject {
+    }
+
+    #inputsubject {
         padding: 7px;
         font-size: 16px;
         border: none;
-        width: 720px;
+        width: 420px;
         border-radius: 5px;
         text-align: left;
-      }
-      input::placeholder {
-        text-align: top;
-      }
-      #inputmessage {
+    }
+
+    input::placeholder {
+        background-color: #eeeeee;
+        border-radius: 10px;
+        font-size: 10px
+    }
+
+    #inputmessage::placeholder {
+        background-color: #eeeeee;
+        border-radius: 10px;
+        font-size: 10px
+    }
+
+    #inputmessage {
         padding: 7px;
         font-size: 16px;
         border: none;
-        width: 720px;
+        width: 420px;
         border-radius: 5px;
         text-align: left;
-        height: 160px;
-      }
-      .row p {
+        height: 120px;
+    }
+
+    .row p {
         padding-bottom: 15px;
         padding-top: 15px;
-      }
-      .add {
+    }
+
+    .add {
         text-align: center;
-      }
-      .email,
-      .phone,
-      .socialmedia,
-      .GPS {
+        background-color: white;
+        padding-top: 38px;
+    }
+
+    .email,
+    .phone,
+    .socialmedia,
+    .GPS {
         padding-top: 60px;
         padding-bottom: 20px;
         margin: 40px;
-      }
-      .submet {
+        background-color: #eeeeee;
+    }
+
+    .submet {
         text-align: center;
         padding-right: 100px;
-        padding-top: 40PX;
-
-      }
-
-      .submet a {
-        background-color: #00aeef;
+        padding-top: 40px;
         border-radius: 10px;
-        color: white ;
-        text-decoration: none;
         padding: 10px;
-      }
+    }
+
+    button[type="submit"] {
+        text-align: center;
+        border-radius: 10px;
+        padding: 10px;
+        border: none;
+        background-color: #00aeef;
+    }
+
+    .submet {}
 </style>
